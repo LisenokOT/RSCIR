485c828ba7fa9aef744227d8ff1a83f5e2dadfc0
Denis Sirotkin <sirotkin@denis-02.ru>
2022-12-14 20:50:34 +0300

    delete 7 tasks

--- pr_7/www/dynamic/index.php
+++ pr_7/www/dynamic/index.php
@@ -1,60 +1,11 @@
 <?php
-// On start
-session_start();
-date_default_timezone_set("Europe/Moscow");
-
-# Get class
-function getClass($path, $classtype)
-{
-    $classpath = $_SERVER['DOCUMENT_ROOT'] . "/{$path}/_MVC.php"; // Задание пути к контроллеру
-    $class = "{$path}{$classtype}"; // Задание пути к классу контроллера
-    if (file_exists($classpath)) {
-        require_once $classpath;
-        if (class_exists($class))
-            return $class;
-    }
-    throw new Exception('Missing Class');
-}
-
-
-$ControllerClass = main();
-
-# Start
-function main()
-{
-    # Redirect on missing files
-    $server_path = $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"];
-    $server_path = strtok($server_path, '?');
-    if (!file_exists($server_path)) {
-        header("location: /home.php");
-        return;
-    };
-    # Test MySQL server
-    try {
-        $connection = new mysqli("database", "user", "password", "appDB");
-    }
-    catch (Exception $e) {
-        echo 'Service not available. Please try again later.';
-        return;
-    }
-    # Current URL
-    $current_url = trim($_SERVER['REQUEST_URI'], '/');
-    $current_url_array = explode('/', $current_url);
-    # Redirect from root directory
-    if (count($current_url_array) == 1) {
-        header('Location: /home.php');
-        return;
-    }
-    # Get classes by URL
-    $ModelClass = getClass($current_url_array[0], 'Model');
-    $ViewClass = getClass($current_url_array[0], 'View');
-    $ControllerClass = getClass($current_url_array[0], 'Controller');
-
-    # Start controller
-    $ControllerClass = new $ControllerClass(
-        $ModelClass,
-        $ViewClass,
-        '/' . $current_url
-    );
-    return $ControllerClass;
-}
+    $title = 'Главная';
+    require "assets/blocks/header.php";
+?>
+<a href="timetable.php">Расписание</a>
+<a href="admin/admin.php">Админ панель</a>
+<a href="storage/files.php">Загрузка pdf файлов</a>
+<a href="index.html">Шахматы</a>
+<?php
+    require "assets/blocks/footer.php";
+?>
